4a8bfe72f8b18eb7ec567cb38f4303a7b2efe751
Eric Ward <ezeward4@gmail.com>
2013-03-29 22:54:33 -0400

    Can accept any number of arguments

--- mathCad/main.cpp
+++ mathCad/main.cpp
@@ -5,10 +5,11 @@
 int main(void)
 {
 	opSymb::loadValid();
-	string expression("3x^2 + 5x - 9");
+	string expression("3x^2 + 5y - 9");
 	Expression *expr = new Expression(expression);
 	Expression::Arg_t *args = new Expression::Arg_t;
 	(*args)['x'] = 3.0;
+	(*args)['y'] = -6.5;
 	cout<<expression<<endl<<expr->evaluate(args)<<endl;
 	system("pause");
 	return 0;
